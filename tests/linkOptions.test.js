59c6c92b74d9aeb76310c65e2aadb20f2655c9b5
DamienBouvet22 <damien.bouvet@le-campus-numerique.fr>
2020-08-28 15:43:06 +0200

    Add tests

--- tests/linkOptions.test.js
+++ tests/linkOptions.test.js
@@ -4,14 +4,24 @@ describe("Sign in and out with valid Credentials", () => {
     let page;
 
     test('sign in', async () => {
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+        });
+        const username = 'Louis';
+        const pwd = 'CK';
         await page.goto('http://polr.alwaysdata.net');
         await page.waitForSelector('#navbar > li.dropdown > a');
         await page.$eval( '#navbar > li.dropdown > a', el => el.click() );
         await page.screenshot({path: './tests/img/displaySignIn.png'});
-        await page.$eval( '#dropdown > form > input:nth-child(1)', el => el.value='admin' );
-        await page.$eval( '#dropdown > form > input:nth-child(2)', el => el.value='campus' );
+        await page.type('#dropdown > form > input:nth-child(1)', username);
+        await page.type('#dropdown > form > input:nth-child(2)', pwd);
+        // await page.$eval( '#dropdown > form > input:nth-child(1)', el => el.value=' ' );
+        //await page.$eval( '#dropdown > form > input:nth-child(2)', el => el.value=' ' );
         await page.screenshot({path: './tests/img/signInCredentials.png'});
         await page.$eval( '#dropdown > form > input.btn.btn-success.form-control.login-form-submit', el => el.click() );
+        const html = await page.$eval('#navbar > div > li > a', e => e.innerHTML);
+        expect(html).toContain(username);
         await page.screenshot({path: './tests/img/signedIn.png'});
     }, timeout);
 
@@ -20,11 +30,9 @@ describe("Sign in and out with valid Credentials", () => {
         await page.waitForSelector('#navbar > div > li > a');
         await page.$eval( '#navbar > div > li > a', el => el.click() );
         await page.screenshot({path: './tests/img/displayLogOut.png'});
-        await page.evaluate( () => {
-            Array
-                .from( document.querySelectorAll( '#navbar > div > li > ul > li' ))
-                .filter( el => el.textContent === 'Logout' )[0].click();
-        });
+        await page.$eval('#navbar > div > li > ul > li:nth-child(3) > a', el => el.click());
+        const html = await page.$eval('#navbar > li.dropdown > a', e => e.innerHTML);
+        expect(html).toContain("Sign In");
         await page.screenshot({path: './tests/img/loggedOut.png'});
     }, timeout);
 
